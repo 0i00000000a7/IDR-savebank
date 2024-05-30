bba8cbd8fe8bdccc592cce2cef77cc6077ac16d3
0i00000000a7 <149916146+0i00000000a7@users.noreply.github.com>
2024-05-30 22:52:16 +0800

    Add files via upload

--- Save.js
+++ Save.js
@@ -4,7 +4,7 @@ class Save {
     this.name = config.name || "未命名存档";
     this.desc = config.desc || "没有存档描述";
     this.data = config.data || "";
-    this.author = config.author || "";
+    this.author = config.author || "0100000a7";
     this.id = NaN
   }
   copy() {
@@ -22,7 +22,7 @@ class Save {
     a.click()
   }
   toFormat() {
-    return `<div class="saves"><span style="font-size: 20px">${this.name}</span><br><span style="font-size: 15px; font-style: italic">${this.desc}</span><br><span style="font-size: 10px">由${this.author}上传</span><br><button class='btn' onclick='Saves[${this.id}].copy()'>粘贴至剪贴板</button> <button class='btn' onclick='Saves[${this.id}].save()'>以.txt形式保存</button></div><br><br>`
+    return `<div class="saves"><span style="font-size: 20px">${this.name}</span><br><span style="font-size: 15px; font-style: italic">${this.desc}</span><br><span style="font-size: 10px">由${this.author}上传</span><br><button class='btn' onclick='saves.categories[${this.categoryId}].saves[${this.id}].copy()'>粘贴至剪贴板</button> <button class='btn' onclick='saves.categories[${this.categoryId}].saves[${this.id}].save()'>以.txt形式保存</button></div><br><br>`
   }
 }
 class Category {
@@ -31,12 +31,40 @@ class Category {
     this.styles = styles || ""
     this.styles += "font-size:24px"
     this.saves = saves
-    for(let i = 0; i < saves.length; i++) this.saves[i].id = i
+    for(let i = 0; i < saves.length; i++) {
+      this.saves[i].id = i
+    }
   }
   applyHTML() {
     let tempHTML = ''
     tempHTML += `<div class="saves" style="${this.styles}"><span>${this.name}</div><br><br>`
     for(let i = 0; i < this.saves.length; i++) tempHTML += this.saves[i].toFormat()
     document.getElementById('saves').innerHTML += tempHTML
+  }
+  toFolderInZipFile() {
+    let folder = fullsaves.folder(`(${this.id+1}) `+this.name)
+    for(let i = 0; i < this.saves.length; i++) folder.file(`(${this.saves[i].id+1}) ${this.saves[i].name}.txt`,this.saves[i].data)
+  }
+  mergeIdToSaves() {
+    for(let i = 0; i < this.saves.length; i++) {
+      this.saves[i].categoryId = this.id
+    }
+  }
+}
+class CategorySaveGroup {
+  constructor(categories) {
+    this.categories = categories
+    for(let i = 0; i < this.categories.length; i++) {
+      this.categories[i].id = i
+      this.categories[i].mergeIdToSaves()
+    }
+  }
+  applyAllHTML() {
+    for(let i = 0; i < this.categories.length; i++) {
+      this.categories[i].applyHTML()
+    }
+  }
+  createZip() {
+    for(let i = 0; i < this.categories.length; i++) this.categories[i].toFolderInZipFile()
   }
 }
--- index.js
+++ index.js
@@ -1,16 +1,29 @@
-pre_world.applyHTML()
-world.applyHTML()
-universe.applyHTML()
-chaos.applyHTML()
-voids.applyHTML()
-theorem.applyHTML()
-reality.applyHTML()
-beyond.applyHTML()
+saves = new CategorySaveGroup([
+  pre_world,
+  world,
+  universe,
+  chaos,
+  voids,
+  theorem,
+  reality,
+  beyond
+])
+saves.applyAllHTML()
 /*待会直接复制粘贴
 new Save({
     name: '开局档',
     desc: '',
     data: '',
-    author: '0100000a7',
   }),
 */
+const fullsaves = new JSZip()
+saves.createZip()
+function DownloadAllSaves() {
+  fullsaves.generateAsync({type:"blob"}).then(function(content) {
+    window.URL = window.URL || window.webkitURL;
+    let a = document.createElement("a")
+    a.href = window.URL.createObjectURL(content)
+    a.download = "所有存档.zip"
+    a.click()
+  });
+}
